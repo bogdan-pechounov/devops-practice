46ca7381ba44639cd091ab359e261e1fdf651682
Bogdan <bogdan.pechounov@gmail.com>
2023-03-07 13:28:10 -0500

    auth + redis

--- node-docker/index.js
+++ node-docker/index.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const redis = require('redis')
+const RedisStore = require('connect-redis').default
 const config = require('./config/config')
 
 const postRouter = require('./routes/postRoutes')
+const userRouter = require('./routes/userRoutes')
 
 //init
 const app = express()
 const port = process.env.PORT || 3000
+// store
+const redisClient = redis.createClient({
+  url: 'redis://redis:6379',
+  // host: config.REDIS_URL,
+  // port: config.REDIS_PORT,
+})
+console.log(config.REDIS_URL)
+redisClient
+  .connect()
+  .then(() => console.log('connected to redis'))
+  .catch(console.error)
+const redisStore = new RedisStore({ client: redisClient })
 
 //connect
 const mongoUrl = `mongodb://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_IP}:${config.MONGO_PORT}?authSource=admin`
@@ -18,11 +34,25 @@ mongoose
 
 //middleware
 app.use(express.json())
+app.use(
+  session({
+    store: redisStore,
+    secret: config.SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30_000,
+    },
+  })
+)
 
 //routes
 app.get('/', (req, res) => {
   res.send('<h1>Hello there3!!!</h1>')
 })
 app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/users', userRouter)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
