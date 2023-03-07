a1e0e324344e4216a64a944f54d64dcb6fc9980d
Bogdan <bogdan.pechounov@gmail.com>
2023-03-07 12:24:36 -0500

    crud api for posts

--- node-docker/index.js
+++ node-docker/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./config/config')
 
-const app = express()
+const postRouter = require('./routes/postRoutes')
 
+//init
+const app = express()
+const port = process.env.PORT || 3000
+
+//connect
 const mongoUrl = `mongodb://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_IP}:${config.MONGO_PORT}?authSource=admin`
 console.log(mongoUrl)
 mongoose
@@ -11,10 +16,13 @@ mongoose
   .then(() => console.log('connected to db'))
   .catch((e) => console.log(e))
 
-const port = process.env.PORT || 3000
+//middleware
+app.use(express.json())
 
+//routes
 app.get('/', (req, res) => {
   res.send('<h1>Hello there3!!!</h1>')
 })
+app.use('/api/v1/posts', postRouter)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
