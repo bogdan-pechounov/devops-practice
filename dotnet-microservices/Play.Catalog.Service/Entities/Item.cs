5008f33c964caff8c5c713aa7d811c9cb9e39972
Bogdan <bogdan.pechounov@gmail.com>
2023-03-09 17:26:10 -0500

    items repository

---
+++ dotnet-microservices/Play.Catalog.Service/Entities/Item.cs
@@ -0,0 +1,12 @@
+public class Item
+{
+  public Guid Id { get; set; }
+
+  public string Name { get; set; }
+
+  public string Description { get; set; }
+
+  public decimal Price { get; set; }
+
+  public DateTimeOffset CreatedDate { get; set; }
+}
